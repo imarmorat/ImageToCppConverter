d3d06f510558cc1125d059ed23ec3763310f5b65
TylerDurden <inthemerdeactually@gmail.com>
2015-12-16 16:14:48 +0000

    initial commit

---
+++ ImageToCppConverter/ImageData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageToCppConverter
+{
+    public class ImageData
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public string Name { get; set; }
+        public int BitsPerColor { get; set; }
+        public List<int> Pixels { get; protected set; } = new List<int>();
+
+        internal static ImageData FromBitmap(Bitmap image)
+        {
+            var imgd = new ImageData()
+            {
+                Height = image.Height,
+                Width = image.Width,
+                //BitsPerColor = image.PixelFormat
+            };
+
+            for (int x = 0; x < imgd.Width; x++)
+                for (int y = 0; y < imgd.Height; y++)
+                    imgd.Pixels.Add(image.GetPixel(x, y).ToArgb());
+
+            return imgd;
+        }
+    }
+}
---
+++ ImageToCppConverter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RazorEngine.Templating;
+using RazorEngine;
+using System.IO;
+
+namespace ImageToCppConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var templatePath = "template1.cppx";
+            var templateOutput = "template1.cpp";
+
+            var image = Bitmap.FromFile("Images/warm 24.png");
+            var data = ImageData.FromBitmap((Bitmap) image);
+            data.Name = "temperatureIcon";
+
+            var templateContent = File.ReadAllText(templatePath);
+            string myParsedTemplate = Engine.Razor.RunCompile(templateContent, "templateNameInTheCache", null, data);
+            File.WriteAllText(templateOutput, myParsedTemplate);
+        }
+    }
+}
